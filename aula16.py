470eccd3d2b9d74a3089fce85a43300ac899cb7f
Pedro Henrique <123213623+HenryPedro@users.noreply.github.com>
2023-12-01 00:40:05 -0300

    Add files via upload

---
+++ aula16.py
@@ -0,0 +1,21 @@
+condicao = True
+
+condicao0 = True
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = True
+
+if condicao1:
+   print('Código para condição 1')
+elif condicao2:   
+  print('Código para condição 2')
+if condicao3:
+      print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('nenhuma condição foi satisfeita')
+ 
+if 10 == 10: 
+    print('outro if')   
